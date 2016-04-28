74449966fb55ebea920e25729e039bfa9dcf972d
Zach Briggs <briggszj@gmail.com>
2016-04-28 18:27:16 -0500

    WIP Implement new fruit sale

--- app/assets/javascripts/components/create_fruit_sale.js
+++ app/assets/javascripts/components/create_fruit_sale.js
@@ -3,7 +3,21 @@
 
   Vue.component("vue-create-fruit-sale", {
 
-    template: "#create-fruit-sale-template"
+    template: "#create-fruit-sale-template",
+
+    data: function () {
+      return {
+        newFruitSale: {}
+      }
+    },
+
+    methods: {
+
+      handleNewFruitSale: function () {
+        console.log("fruit sale handled, yo")
+      }
+
+    }
 
   })
 })();
