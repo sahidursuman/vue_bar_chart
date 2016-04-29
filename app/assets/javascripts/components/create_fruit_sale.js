873b4efea1b58ff2583f353ff1bb40f77c250a8d
Zach Briggs <briggszj@gmail.com>
2016-04-28 23:35:47 -0500

    Get fruit stats when creating fruit sales

--- app/assets/javascripts/components/create_fruit_sale.js
+++ app/assets/javascripts/components/create_fruit_sale.js
@@ -7,14 +7,44 @@
 
     data: function () {
       return {
-        newFruitSale: {}
+        newFruitSale: {},
+        fruitStats: []
       }
     },
 
+    ready: function () {
+      var self = this
+
+      self.fetchFruitStats()
+    },
     methods: {
 
+      fetchFruitStats: function () {
+        var self = this
+
+        $.ajax({
+          method: "GET",
+          url: "/api/fruit_stats"
+        }).then(function (data) {
+          self.fruitStats = data
+          console.log("fruit stats gotten!")
+        })
+      },
+
       handleNewFruitSale: function () {
-        console.log("fruit sale handled, yo")
+        var self = this
+
+        // Guard against empty variety field
+        if (!self.newFruitSale.variety) { return }
+
+        $.ajax({
+          method: "POST",
+          url: "/api/fruit_sales",
+          data: { fruit_sale:  self.newFruitSale }
+        }).then(function (data) {
+          self.fetchFruitStats()
+          console.log("created!", data)
+        })
       }
 
     }
